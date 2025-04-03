3cce2931116692879c82119f11327be1057ce5e5
diegonms <diegonms06@gmail.com>
2025-04-03 14:16:40 -0300

    Lado esquerdo da tela de login feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Login from "./components/login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </Router>
   );
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const login = () => {
-  return <div></div>;
-};
+export default function Login() {
+  return (
+    <div className="login-container">
+      <div className="login">
+        <img
+          className="logo-login"
+          src="../idVisual/logotipoPreto.svg"
+          alt="Logo"
+        />
+        <div className="container-input">
+          <h1 className="login-title">Login</h1>
+          <p className="text-muted">Faça o seu login na nossa conta</p>
 
-export default login;
+          <div className="input-group mb-3">
+            <label className="label-input">Email ou nome de Usuário</label>
+            <input
+              type="text"
+              className="input-user"
+              placeholder="Usuário"
+              aria-label="Usuário"
+              aria-describedby="basic-addon2"
+            />
+          </div>
+
+          <div className="input-group senha mb-3">
+            <label className="label-input">Senha</label>
+            <input
+              type="password"
+              className="input-password "
+              placeholder="Senha"
+              aria-label="Senha"
+              aria-describedby="basic-addon2"
+            />
+          </div>
+
+          <button className="entrar-btn">LogIn</button>
+        </div>
+      </div>
+    </div>
+  );
+}
